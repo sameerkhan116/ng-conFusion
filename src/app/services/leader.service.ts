6a4acb219857526abed8b38d44854ce4f4ae87ff
Sameer Khan <sameer_khan@student.uml.edu>
2017-11-05 19:58:54 -0500

    Completed week 2 assignment

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -8,4 +8,16 @@ export class LeaderService {
 
   constructor() { }
 
+  getLeaders(): Leader[] {
+  	return LEADERS;
+  }
+
+  getLeader(id: number): Leader {
+  	return LEADERS.filter((leader) => (leader.id === id))[0];
+  }
+
+  getFeaturedLeader(): Leader {
+  	return LEADERS.filter((leader) => (leader.featured))[0];
+  }
+
 }
