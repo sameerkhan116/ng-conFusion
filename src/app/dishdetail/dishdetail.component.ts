079e35137edd617b774f72402590408b9f901626
Sameer Khan <sameer_khan@student.uml.edu>
2017-11-09 23:33:43 -0500

    Updated submission with rating form

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -8,6 +8,10 @@ import { DishService } from '../services/dish.service';
 
 import 'rxjs/add/operator/switchMap';
 
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Rating } from '../shared/rating';
+import { DISHES } from '../shared/dishes';
+
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
@@ -18,11 +22,36 @@ export class DishdetailComponent implements OnInit {
 	dish: Dish;
   dishIds: number[];
   prev: number;
-  next: number;
+  next: number
+  ratingForm: FormGroup;
+  rat: Rating;
+
+  formErrors = {
+    'author': '',
+    'rating': '',
+    'comment': ''
+  };
+
+  validationMessages = {
+    'author': {
+      'required': 'Your name is required',
+      'minlength': 'Your name must be atleast 2 character long',
+      'maxlength': 'Your name cannot be more than 25 characters long'
+    },
+    'rating': {
+      'required': 'Rating is required',
+    },
+    'comment': {
+      'required': 'Your comment is required'
+    }
+  }
 
 	constructor(private dishservice: DishService,
 		private route: ActivatedRoute,
-		private location: Location) { }
+		private location: Location,
+    private rt: FormBuilder) { 
+      this.createForm();
+  }
 
   	ngOnInit() {
       this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
@@ -40,4 +69,47 @@ export class DishdetailComponent implements OnInit {
   	goBack(): void {
   		this.location.back();
   	}
+
+    createForm(): void {
+      this.ratingForm = this.rt.group({
+        author: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+        rating: '',
+        comment: ['', [Validators.required]]
+      });
+
+      this.ratingForm.valueChanges
+        .subscribe(data => this.onValueChanged(data));
+
+      this.onValueChanged();
+    }
+
+    onValueChanged(data?: any) {
+      if(!this.ratingForm) {
+        return;
+      }
+      const form = this.ratingForm;
+
+      for(const field in this.formErrors) {
+        this.formErrors[field] = '';
+
+        const control = form.get(field);
+        if(control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
+          for(const key in control.errors) {
+            this.formErrors[field] += messages[key] + ' ';
+          }
+        }
+      }
+    }
+
+    onSubmit() {
+      this.rat = this.ratingForm.value;
+      console.log(this.rat);
+      this.ratingForm.reset({
+        author: '',
+        rating: '5',
+        comment: ''
+      });
+    }
+
 }
---
+++ src/app/shared/rating.ts
@@ -0,0 +1,5 @@
+export class Rating {
+	author: string;
+	rating: number;
+	comment: string;
+}
